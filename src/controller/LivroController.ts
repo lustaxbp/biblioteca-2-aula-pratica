760db7e46d382f2df1c01b83c140965d269aff68
Luís Bordin <luisbordinpereira@gmail.com>
2025-05-21 15:45:08 -0300

    affs

--- src/controller/LivroController.ts
+++ src/controller/LivroController.ts
@@ -1,5 +1,7 @@
+import path from "path";
 import { Livro } from "../model/Livro";
 import { Request, Response} from "express";
+import fs from 'fs';
 
 /**
  * Interface LivroDTO
@@ -48,11 +50,11 @@ class LivroController extends Livro {
     static async cadastrar(req: Request, res: Response) {
         try {
             const dadosRecebidos: LivroDTO = req.body;
-            
+
             // Instanciando objeto Livro
             const novoLivro = new Livro(
                 dadosRecebidos.titulo,
-                dadosRecebidos.autor, 
+                dadosRecebidos.autor,
                 dadosRecebidos.editora,
                 (dadosRecebidos.anoPublicacao ?? 0).toString(),
                 dadosRecebidos.isbn ?? '',
@@ -65,15 +67,39 @@ class LivroController extends Livro {
             // Chama o método para persistir o livro no banco de dados
             const result = await Livro.cadastrarLivro(novoLivro);
 
-            // Verifica se a query foi executada com sucesso
-            if (result) {
-                return res.status(200).json(`Livro cadastrado com sucesso`);
+            // Verifica se o cadastro foi bem-sucedido
+            if (result.queryResult && result.idLivro) {
+                novoLivro.setIdLivro(result.idLivro);
+
+                // Inserindo capa do livro, se informada
+                if (req.file) {
+                    const ext = path.extname(req.file.originalname);
+                    const sanitize = (texto: string) => texto
+                        .replace(/[^a-zA-Z0-9-_ ]/g, '') // remove caracteres especiais (exceto espaço, hífen e underscore)
+                        .replace(/ /g, "_");             // troca espaços por underscores
+
+                    const tituloSanitizado = sanitize(novoLivro.getTitulo());
+                    const editoraSanitizada = sanitize(novoLivro.getEditora());
+
+                    const novoNome = `${tituloSanitizado}_${editoraSanitizada}${ext}`;
+                    const antigoPath = req.file.path;
+                    const novoPath = path.resolve(req.file.destination, novoNome);
+
+                    fs.renameSync(antigoPath, novoPath);
+
+                    await Livro.atualizarImagemCapa(novoNome, novoLivro.getIdLivro());
+                }
+
+                // Retorno de sucesso com o ID do livro
+                return res.status(200).json({mensagem: 'Livro cadastrado com sucesso'});
             } else {
-                return res.status(400).json('Não foi possível cadastrar o livro no banco de dados');
+                return res.status(400).json({mensagem: 'Não foi possível cadastrar o livro no banco de dados'});
             }
         } catch (error) {
-            console.log(`Erro ao cadastrar o livro: ${error}`);
-            return res.status(400).json('Erro ao cadastrar o livro');
+            console.error(`Erro ao cadastrar o livro: ${error}`);
+            return res.status(500).json({
+                mensagem: 'Erro ao cadastrar o livro'
+            });
         }
     }
 
